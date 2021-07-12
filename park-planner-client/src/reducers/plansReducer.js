9821bf6372a7c6b34dce022cd0711b90dc9102f2
mccleskeyc <mccleskeyc@gmail.com>
2021-07-12 15:45:26 -0400

    redux set up

---
+++ park-planner-client/src/reducers/plansReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    plans: [],
+    loading: true
+}
+const plansReducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default plansReducer
