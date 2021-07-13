e7316127052ebed506b70f848efc2e23a1021182
mccleskeyc <mccleskeyc@gmail.com>
2021-07-13 10:34:53 -0400

    added epcot lands routing

--- park-planner-client/src/App.js
+++ park-planner-client/src/App.js
@@ -12,6 +12,19 @@ import Studios from './components/org/parks/Studios'
 import Form from './components/utility/Form'
 import Index from './components/utility/Index'
 import FutureWorld from "./components/org/lands/Epcot/FutureWorld";
+import AmericanAdventure from "./components/org/lands/Epcot/AmericanAdventure";
+import Canada from "./components/org/lands/Epcot/Canada";
+import China from "./components/org/lands/Epcot/China";
+import France from "./components/org/lands/Epcot/France";
+import Germany from "./components/org/lands/Epcot/Germany";
+import Italy from "./components/org/lands/Epcot/Italy";
+import Japan from "./components/org/lands/Epcot/Japan";
+import Mexico from "./components/org/lands/Epcot/Mexico";
+import Morocco from "./components/org/lands/Epcot/Morocco";
+import Norway from "./components/org/lands/Epcot/Norway";
+import UK from "./components/org/lands/Epcot/UK";
+import WorldShowcaseOther from "./components/org/lands/Epcot/WorldShowcaseOther";
+
 
 class App extends Component {
 
@@ -29,15 +42,29 @@ class App extends Component {
     <Router>
       <Nav />
         <Switch>
+        {/* main nav */}
           <Route exact path="/" component={Home} />
           <Route exact path="/attractions" component={Attraction} />
           <Route exact path="/food" component={Food} />
+          <Route exact path="/form" component={Form} />
+          <Route exact path="/index" component={Index} />
+        {/* parks nav */}
           <Route exact path="/mk" component={MK} />
           <Route exact path="/epcot" component={Epcot} />
           <Route exact path="/studios" component={Studios} />
-          <Route exact path="/form" component={Form} />
-          <Route exact path="/index" component={Index} />
           <Route exact path="/futureworld" component={FutureWorld} />
+          <Route exact path="/americanadventure" component={AmericanAdventure} />
+          <Route exact path="/canada" component={Canada} />
+          <Route exact path="/china" component={China} />
+          <Route exact path="/france" component={France} />
+          <Route exact path="/germany" component={Germany} />
+          <Route exact path="/italy" component={Italy} />
+          <Route exact path="/japan" component={Japan} />
+          <Route exact path="/mexico" component={Mexico} />
+          <Route exact path="/morocco" component={Morocco} />
+          <Route exact path="/norway" component={Norway} />
+          <Route exact path="/uk" component={UK} />
+          <Route exact path="/worldshowcase" component={WorldShowcaseOther} />
         </Switch>
     </Router>
   );
--- park-planner-client/src/components/org/lands/Epcot/UK.js
+++ park-planner-client/src/components/org/lands/Epcot/UK.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import Plan from '/Users/cmccleskey/Development/code/personal/park-planner/park-planner-client/src/components/utility/Plan'
 export class UK extends Component {
-    rrender() {
+    render() {
         const plans = this.props.plans.map((plan, i) => <Plan key={i}name={plan.name} category={plan.category} park={plan.park} land={plan.land} priority={plan.priority} notes={plan.notes}/>)
 
         const filteredPlans = plans.filter(plan => plan.props.land === "United Kingdom" && plan.props.park === "Epcot")
