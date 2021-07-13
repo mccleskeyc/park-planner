e1a74a8b9b443f117234b0ecf2b56cee20f8a42d
mccleskeyc <mccleskeyc@gmail.com>
2021-07-13 10:44:24 -0400

    attractions component

--- park-planner-client/src/components/org/Attractions.js
+++ park-planner-client/src/components/org/Attractions.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import Plan from '/Users/cmccleskey/Development/code/personal/park-planner/park-planner-client/src/components/utility/Plan'
 
 class Attractions extends Component {
     render() {
+        const plans = this.props.plans.map((plan, i) => <Plan key={i}name={plan.name} category={plan.category} park={plan.park} land={plan.land} priority={plan.priority} notes={plan.notes}/>)
+
+        const filteredPlans = plans.filter(plan => plan.props.category === "Attraction")
         return (
             <div>
-                All Attractions
+                <h1><u>All Epcot Plans</u></h1>
+                <div>
+                    {filteredPlans}
+                </div>
             </div>
         )
     }
 }
+const mapStateToProps = state => {
+    return {
+        plans: state.plans
+    }
+}
 
-export default Attractions
+export default connect(mapStateToProps)(Attractions)
