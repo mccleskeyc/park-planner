b5154183a29e991d6172c9dda14a0f78585d716a
mccleskeyc <mccleskeyc@gmail.com>
2021-07-13 21:02:19 -0400

    logo

--- park-planner-client/src/components/utility/Nav.js
+++ park-planner-client/src/components/utility/Nav.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '/Users/cmccleskey/Development/code/personal/park-planner/park-planner-client/src/logo.png'
 export class Nav extends Component {
     render() {
         return (
             <div className="nav">
+                
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/attractions">Attractions</Link></li>
-                    <li><Link to="/food">Food</Link></li>
-                    <li><Link to="/mk">Magic Kingdom</Link></li>
-                    <li><Link to="/epcot">Epcot</Link></li>
-                    <li><Link to="/studios">Studios</Link></li>
-                    <li><Link to="/form">Make a Plan</Link></li>
-                    <li><Link to="/index">All Plans</Link></li>
+                <img src={Logo} className="logo" />
+                    <li><Link to="/" className="link">Home</Link></li>
+                    <li><Link to="/attractions"className="link">Attractions</Link></li>
+                    <li><Link to="/food" className="link">Food</Link></li>
+                    <li><Link to="/mk" className="link">Magic Kingdom</Link></li>
+                    <li><Link to="/epcot" className="link">Epcot</Link></li>
+                    <li><Link to="/studios" className="link">Studios</Link></li>
+                    <li><Link to="/form" className="link">Make a Plan</Link></li>
+                    <li><Link to="/index" className="link">All Plans</Link></li>
                 </ul>
             </div>
         )
