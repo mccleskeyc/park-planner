58f19f95b6b9745880fad578481ec11e4c886327
mccleskeyc <mccleskeyc@gmail.com>
2021-07-14 10:10:13 -0400

    added home navigation

--- park-planner-client/src/components/utility/Home.js
+++ park-planner-client/src/components/utility/Home.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react'
-
+import lands from './imgs/lands.jpeg'
+import parks from './imgs/parks.jpeg'
+import categories from './imgs/categories.png'
+import { Link } from 'react-router-dom'
 export class Home extends Component {
     render() {
         return (
             <div className="container">
                 <div className="content">
                     <h1>Park Planner</h1>
-                    <p>Welcome to Park Planner! Plan your upcoming WDW trip here.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel tincidunt velit. Fusce elementum ut ex et pellentesque. Integer urna dui, placerat eu neque non, sodales egestas dui. Nullam maximus sollicitudin velit ac pellentesque. Nulla erat erat, lacinia in malesuada sit amet, molestie ac erat. Maecenas risus massa, sodales a ullamcorper ac, consectetur a augue. Aliquam interdum tellus vel lectus hendrerit ornare. </p>
-
-                    <p>In id tincidunt diam. Phasellus malesuada mattis ultrices. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed vitae pulvinar magna, at hendrerit nisl. Integer rutrum bibendum metus vel mollis. Integer condimentum lobortis sagittis. Aenean tristique tincidunt vehicula. In hac habitasse platea dictumst. Morbi at rhoncus augue, sed pulvinar ante. Praesent dictum fringilla lectus, a molestie urna dignissim sit amet. Pellentesque vel lectus et massa suscipit varius ut id mauris. Suspendisse sodales leo lorem, at faucibus nisl maximus nec.</p>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel tincidunt velit. Fusce elementum ut ex et pellentesque. Integer urna dui, placerat eu neque non, sodales egestas dui. Nullam maximus sollicitudin velit ac pellentesque. Nulla erat erat, lacinia in malesuada sit amet, molestie ac erat. Maecenas risus massa, sodales a ullamcorper ac, consectetur a augue. Aliquam interdum tellus vel lectus hendrerit ornare. </p>
-
-                    <p>In id tincidunt diam. Phasellus malesuada mattis ultrices. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed vitae pulvinar magna, at hendrerit nisl. Integer rutrum bibendum metus vel mollis. Integer condimentum lobortis sagittis. Aenean tristique tincidunt vehicula. In hac habitasse platea dictumst. Morbi at rhoncus augue, sed pulvinar ante. Praesent dictum fringilla lectus, a molestie urna dignissim sit amet. Pellentesque vel lectus et massa suscipit varius ut id mauris. Suspendisse sodales leo lorem, at faucibus nisl maximus nec.</p>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel tincidunt velit. Fusce elementum ut ex et pellentesque. Integer urna dui, placerat eu neque non, sodales egestas dui. Nullam maximus sollicitudin velit ac pellentesque. Nulla erat erat, lacinia in malesuada sit amet, molestie ac erat. Maecenas risus massa, sodales a ullamcorper ac, consectetur a augue. Aliquam interdum tellus vel lectus hendrerit ornare. </p>
-
-                    <p>In id tincidunt diam. Phasellus malesuada mattis ultrices. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed vitae pulvinar magna, at hendrerit nisl. Integer rutrum bibendum metus vel mollis. Integer condimentum lobortis sagittis. Aenean tristique tincidunt vehicula. In hac habitasse platea dictumst. Morbi at rhoncus augue, sed pulvinar ante. Praesent dictum fringilla lectus, a molestie urna dignissim sit amet. Pellentesque vel lectus et massa suscipit varius ut id mauris. Suspendisse sodales leo lorem, at faucibus nisl maximus nec.</p>
+                    <h2>What Would You Like To Search By?</h2>
+               <div className="img-navs">
+                    <Link to="/parks"><img src={parks} alt='parks' className="img-nav" /></Link>
+                    <Link to="/lands"><img src={lands} alt='lands' className="img-nav" /></Link>
+                    <Link to="/categories"><img src={categories} alt='categories' className="img-nav" /></Link>
+                </div> 
                 </div>
+                
             </div>
         )
     }
