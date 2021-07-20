a455327733c13c662ee7839cb66f7ad2eae4b1cc
mccleskeyc <mccleskeyc@gmail.com>
2021-07-20 19:00:59 -0400

    fixed Lands styling

--- park-planner-client/src/components/org/lands/Lands.js
+++ park-planner-client/src/components/org/lands/Lands.js
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom'
 export class Lands extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
+            <div className="content">
+            <h1>Lands</h1>
                     <Link to="/mk">Magic Kingdom</Link>
                     **insert MK lands icons here**
                     <Link to="/epcot">Epcot</Link>
                     **insert epcot lands icons here**
                     <Link to="/studios">Hollywood Studios</Link>
                     **insert studios lands icons here**
+            </div>
             </div>
         )
     }
