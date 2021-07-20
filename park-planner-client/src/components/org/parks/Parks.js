f5d558c90d6bc0240041987442268d09d8b124d3
mccleskeyc <mccleskeyc@gmail.com>
2021-07-20 19:01:47 -0400

    fixed Parks styling

--- park-planner-client/src/components/org/parks/Parks.js
+++ park-planner-client/src/components/org/parks/Parks.js
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom'
 export class Parks extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
+            <div className="content">
+            <h1>All Parks</h1>
                     <Link to="/mk">Magic Kingdom</Link>
                     <Link to="/epcot">Epcot</Link>
                     <Link to="/studios">Hollywood Studios</Link>
+            </div>
             </div>
         )
     }
