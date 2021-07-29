6256c990f849e87e0bd8913d15bbd9713053c935
mccleskeyc <mccleskeyc@gmail.com>
2021-07-29 17:24:34 -0400

    updated studios component

--- park-planner-client/src/components/org/parks/Studios.js
+++ park-planner-client/src/components/org/parks/Studios.js
@@ -7,14 +7,18 @@ class Studios extends Component {
         const plans = this.props.plans.map((plan, i) => <Plan key={i}name={plan.name} category={plan.category} park={plan.park} land={plan.land} priority={plan.priority} notes={plan.notes}/>)
 
         const filteredPlans = plans.filter(plan => plan.props.park === "Hollywood Studios")
+        const foodPlans = filteredPlans.filter(plan => plan.props.category === "Food - Restaurant/Stall" || plan.props.category === "Food - Item")
+        const attractionPlans = filteredPlans.filter(plan => plan.props.category === "Attraction" || plan.props.category === "None/Other")
         if (filteredPlans.length > 0) {
             return (
                 <div className="container">
                 <div className="content">
-                    <h1>Hollywood Studios Plans</h1>
-                        {filteredPlans}
-                        </div>
-                </div>
+                    <h1>Epcot Plans</h1>
+                    <h2>Attractions & Events</h2>
+                    {attractionPlans}
+                    <h2>Food</h2>
+                    {foodPlans}
+                </div></div>
             )} else 
             return (
                 <div className="container">
