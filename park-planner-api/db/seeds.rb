a1ad4003e7ec4626dcf3cd563b95565ec7179bb0
mccleskeyc <mccleskeyc@gmail.com>
2021-07-29 17:22:17 -0400

    added to db

--- park-planner-api/db/seeds.rb
+++ park-planner-api/db/seeds.rb
@@ -11,6 +11,131 @@ plans = Plan.create([
 
 # Epcot / Thursday
 
+    {name: "Soarin' Around the World", 
+
+    category: "Attraction", 
+    park: "Epcot", land: "Future World", 
+    priority: "Must", 
+    notes:"Rope Drop"},
+
+# Food & Wine
+
+    {name: "Appleseed Orchard", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Epcot", land: "World Showcase - Other", 
+    priority: "High", 
+    notes:"Frozen Apple Pie & Cinnamon Apple Cider"},
+
+    {name: "Canada Marketplace", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Epcot", land: "Canada", 
+    priority: "Must", 
+    notes:"Le Cellier Filet & Cheddar Cheese Soup"},
+
+    {name: "China Marketplace", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Epcot", land: "China", 
+    priority: "Must", 
+    notes:"- Pan-fried Chicken Dumplings with House-made Sweet and Spicy Sauce
+    - ZiRan Beef Bao Bun: Grilled Beef with Cumin
+    - Crispy-fried Pepper Shrimp with Spicy Sichuan Noodles"},
+
+    {name: "Donut Box", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Epcot", land: "World Showcase - Other", 
+    priority: "High", 
+    notes:"Candy Jar Donut" },
+
+    {name: "Flavors from the Fire (NFL)", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Epcot", land: "World Showcase - Other", 
+    priority: "Medium", 
+    notes:"The Smores Whoopie Pie-lon"},
+
+    {name: "France Marketplace", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Epcot", land: "France", 
+    priority: "Must", 
+    notes:"La Passion Martini Slushy: Vodka, Grey Goose Le Citron Vodka, Cranberry and Passion Fruit Juice // - Beignet aux Trois Fromages: Warm Beignet filled with 3 Cheeses
+
+    - Croissant aux Escargots: Escargot Croissant with Garlic and Parsley,
+    
+    -Coq au Vin, Pommes Dauphine: Chicken braised in Burgundy Wine with Bacon and Puffed Potatoes,
+    
+    -Crème Brûlée au Grand Marnier: Vanilla Crème Brûlée with Grand Marnier Liqueur (contains alcohol) "},
+
+    {name: "Hawaii Marketplace", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Epcot", land: "World Showcase - Other", 
+    priority: "Medium", 
+    notes:"Aulani Sunrise - Vodka, Pineapple Juice, Grenadine"},
+
+    {name: "Hops and Barley", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Epcot", land: "World Showcase - Other", 
+    priority: "Low", 
+    notes:"Lobster Roll"},
+
+    {name: "Shimmering Sips", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Epcot", land: "World Showcase - Other", 
+    priority: "High", 
+    notes:"Key Lime Mimosa with Cranberry"},    
+
+# Other
+
+    {name: "Spaceship Earth", 
+
+    category: "Attraction", 
+    park: "Epcot", land: "Future World", 
+    priority: "Must", 
+    notes:""},
+
+    {name: "Living with the Land", 
+
+    category: "Attraction", 
+    park: "Epcot", land: "Future World", 
+    priority: "Must", 
+    notes:""},
+
+    {name: "Journey into Imagination with Figment", 
+
+    category: "Attraction", 
+    park: "Epcot", land: "Future World", 
+    priority: "Low", 
+    notes:""},
+
+    {name: "Gran Fiesta Tour", 
+
+    category: "Attraction", 
+    park: "Epcot", land: "Mexico", 
+    priority: "Must", 
+    notes:""},
+
+
+    {name: "Test Track", 
+
+    category: "Attraction", 
+    park: "Epcot", land: "Future World", 
+    priority: "Low", 
+    notes:"If I am feeling brave"},
+
+    {name: "The Seas with Nemo & Friends", 
+
+    category: "Attraction", 
+    park: "Epcot", land: "Future World", 
+    priority: "Must", 
+    notes:"Near dinner"},
+
     {name: "Coral Reef", 
 
     category: "Food - Restaurant/Stall", 
@@ -18,12 +143,20 @@ plans = Plan.create([
     priority: "Must", 
     notes:"Reservation for 2 - 8/19 @ 4:45pm"},
 
+    {name: "Gelateria Toscana", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Epcot", land: "Italy", 
+    priority: "Low", 
+    notes:"Fireworks Dessert"},
+
     {name: "Fireworks - Epcot Forever", 
 
     category: "None/Other", 
     park: "Epcot", land: "None/Other", 
     priority: "Must", 
     notes:"Fireworks show @ 9pm on 8/19 - can be done from anywhere, but thinking about getting gelato and watching from Italy"},    
+
 
 # Magic Kingdom / Friday
     {name: "Space Mountain", 
@@ -57,7 +190,7 @@ plans = Plan.create([
     {name: "Dole Whip", 
 
     category: "Food - Item", 
-    park: "Magic Kingdom", land: "Tomorrowland", 
+    park: "Magic Kingdom", land: "Adventureland", 
     priority: "Must", 
     notes:"Aloha Aisle, mobile orders accepted"},
 
@@ -173,6 +306,13 @@ plans = Plan.create([
     priority: "Low", 
     notes:""},
 
+    {name: "Mickey Bar & Sandwich", 
+
+    category: "Food - Item", 
+    park: "Magic Kingdom", land: "None/Other", 
+    priority: "Low", 
+    notes:"Snack carts"},
+
     {name: "Plaza Restaurant", 
 
     category: "Food - Restaurant/Stall", 
@@ -195,6 +335,90 @@ plans = Plan.create([
     park: "Resorts", land: "None/Other", 
     priority: "Medium", 
     notes:"Reservation for 2 - 8/21 @ 7:45am at Old Key West Resort"},
+    
+    {name: "Lightning McQueen's Racing Academy", 
+
+    category: "Attraction", 
+    park: "Hollywood Studios", land: "Sunset Boulevard", 
+    priority: "Low", 
+    notes:""},
+
+    {name: "Mickey & Minnie's Runaway Railway", 
+
+    category: "Attraction", 
+    park: "Hollywood Studios", land: "Hollywood Boulevard", 
+    priority: "High", 
+    notes:"Long lines, so do it earlier in the day"},
+
+    {name: "Millennium Falcon: Smugglers Run", 
+
+    category: "Attraction", 
+    park: "Hollywood Studios", land: "Star Wars: Galaxy's Edge", 
+    priority: "Must", 
+    notes:"Rope Drop"},
+
+    {name: "Star Wars: Galaxy's Edge", 
+
+    category: "Attraction", 
+    park: "Hollywood Studios", land: "Star Wars: Galaxy's Edge", 
+    priority: "Low", 
+    notes:"Need to join queue at 7 and 1"},
+
+    {name: "Star Tours", 
+
+    category: "Attraction", 
+    park: "Hollywood Studios", land: "Echo Lake", 
+    priority: "Medium", 
+    notes:""},
+
+    {name: "MuppetVision", 
+
+    category: "Attraction", 
+    park: "Hollywood Studios", land: "Grand Avenue", 
+    priority: "Medium", 
+    notes:""},
+
+    {name: "Alien Swirling Saucers", 
+
+    category: "Attraction", 
+    park: "Hollywood Studios", land: "Toy Story Land", 
+    priority: "Medium", 
+    notes:""},
+
+    {name: "Toy Story Mania", 
+
+    category: "Attraction", 
+    park: "Hollywood Studios", land: "Toy Story Land", 
+    priority: "Must", 
+    notes:""},
+
+    {name: "Anaheim Produce", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Hollywood Studios", land: "Sunset Boulevard", 
+    priority: "Medium", 
+    notes:"Mickey Pretzels & Ice Creams"},
+
+    {name: "Trolley Car Cafe", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Hollywood Studios", land: "Hollywood Boulevard", 
+    priority: "High", 
+    notes:"Carrot Cake Cookies, also has starbucks drinks including cold brew"},
+
+    {name: "Oga's Cantina", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Hollywood Studios", land: "Star Wars: Galaxy's Edge", 
+    priority: "Medium", 
+    notes:"Bar with non-alcoholic options"},
+
+    {name: "Sci-Fi Dine In", 
+
+    category: "Food - Restaurant/Stall", 
+    park: "Hollywood Studios", land: "Commisary Lane", 
+    priority: "High", 
+    notes:"If we can get in"},
 
     {name: "Enzo's Hideaway", 
 
--- park-planner-client/src/components/utility/Plan.js
+++ park-planner-client/src/components/utility/Plan.js
@@ -6,13 +6,12 @@ export class Plan extends Component {
         const {name, category, park, land, priority, notes, date} = this.props;
         return (
             <div>
-                <h3>{name}</h3>
+                <h3 className="centered-text">✨ {name} ✨</h3>
                 <p><b>Category: </b>{category}</p>
                 <p><b>Park: </b>{park}</p>
                 <p><b>Land: </b>{land}</p>
                 <p><b>Priority: </b>{priority}</p>
                 <p><b>Notes: </b>{notes}</p>
-                <p className="centered-text">✨✨✨</p>
             </div>
         )
     }
