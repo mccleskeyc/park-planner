a0a151fcd2f4f374903a31104122173ea5258119
mccleskeyc <mccleskeyc@gmail.com>
2021-08-05 16:41:26 -0400

    card styling

--- park-planner-client/src/components/utility/Plan.js
+++ park-planner-client/src/components/utility/Plan.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 
 export class Plan extends Component {
-    r
     render() {
         const {name, category, park, land, priority, notes, date} = this.props;
         return (
-            <div>
+            <div className="plan">
                 <h3 className="centered-text">✨ {name} ✨</h3>
                 <p><b>Category: </b>{category}</p>
                 <p><b>Park: </b>{park}</p>
@@ -13,6 +12,7 @@ export class Plan extends Component {
                 <p><b>Priority: </b>{priority}</p>
                 <p><b>Notes: </b>{notes}</p>
             </div>
+            
         )
     }
 }
